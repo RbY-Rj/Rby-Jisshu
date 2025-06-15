4e410a9e4748364debac86c9b8afa6f1fb5647ff
Manicka Raj <96897008+RbY-Rj@users.noreply.github.com>
2025-06-15 22:06:29 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -22,7 +22,7 @@ UPDATE_CAPTION = """<b><blockquote>📫 𝖭𝖤𝖶 𝖥𝖨𝖫𝖤 𝖠𝖣�
 <blockquote>🚀 Telegram Files ✨</blockquote>
 
 {}
-<blockquote>〽️ Powered by @Jisshu_bots</b></blockquote>
+<blockquote>〽️ Powered by @RbyLinkzz</b></blockquote>
 """
 
 QUALITY_CAPTION = """📦 {} : {}\n"""
